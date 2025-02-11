13edbb2f4f1729c103151ac372c0fc211d20875f
Fasterino <166503398+Rfn18@users.noreply.github.com>
2025-02-11 23:49:46 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 let envelope = document.querySelector(".container-envelope");
-let active = true;
-
-// envelope.addEventListener("click", () => {
-//   if (active) {
-//     envelope.classList.add("open");
-//     active = false;
-//     console.log("Active");
-//   } else {
-//     envelope.classList.remove("open");
-//     envelope.classList.add("close");
-//     active = true;
-//     console.log("False");
-//   }
-// });
 
 function Open() {
   envelope.classList.add("open");
+}
+
+function Reset() {
+  envelope.classList.remove("open");
+  envelope.classList.add("close");
 }
